c6b6a1b66187998fde2b9a5fc331783a9a955481
unknown <jfalytiana@gmail.com>
2024-11-12 14:41:29 +0300

    shadcn@latest init et shadcn@latest add button pour NotFound

--- src/component/not-found/index.tsx
+++ src/component/not-found/index.tsx
@@ -1,4 +1,5 @@
 import styles from './NotFound.module.css';
+import { Button } from "@/components/ui/button"
 import { Link } from 'react-router-dom';
 
 function NotFound () {
@@ -6,9 +7,11 @@ function NotFound () {
         <div className={styles.container}>
             <h1 className={styles.title}>404</h1>
             <p className={styles.message}>{"Oups ! La page que vous recherchez n'existe pas."}</p>
-            <Link to="/" className="btn btn2">{"Retour à l'accueil"}</Link>
+            <Button className='text-md px-6 py-7'><Link to="/">{"Retour à l'accueil"}</Link></Button>
+            
         </div>
     );
 };
 
 export default NotFound;
+
